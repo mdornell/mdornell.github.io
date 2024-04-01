5a55496f728964d478f4e6aef78cb3b827312b4a
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-03-31 21:44:38 -0300

    Atualizando dados

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,6 @@
+// OBS : O arquivo js só foi foito pois estarei ultilizando o projeto também como portifolio em meu github 
+// Então já estou fazendo algo mais completo
+
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
